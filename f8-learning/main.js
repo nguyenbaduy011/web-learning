f4057a0d81a4ac8cb853065beb732a2455d700b9
nguyenbaduy011 <nguyenbaduy011@gmail.com>
2024-05-29 21:06:59 +0700

    4th commit

--- f8-learning/main.js
+++ f8-learning/main.js
@@ -43,6 +43,85 @@
 //     console.log('expressionFunction')
 // }
 
-var myname = 'Nguyen Ba Duy'
+// var myName = 'Nguyen Ba Duy'
 
-console.log(myname.length)
+// console.log(myName.length)
+
+// const email = 'email'
+
+
+
+
+
+
+
+// var myName = 'Nguyen Ba Duy'
+// console.log(myName.indexOf('Ba', 10))
+// console.log(myName.slice(4, 10))
+
+// function getUpperCaseName(name)
+// {
+//     return name.toUpperCase()
+// }
+    
+// // Expected results:
+// console.log(getUpperCaseName("Nguyen van a")) // "NGUYEN VAN A"
+// console.log(getUpperCaseName("nGuyen vAn C")) // "NGUYEN VAN C"
+
+
+
+// function isNumber(value){
+//     if (value === Number(value))
+//     return true    
+//     else return false
+// }
+
+// // Expected results:
+// console.log(isNumber(999)); // true
+// console.log(isNumber('abc')); // false
+// console.log(isNumber('100')); // false
+
+// var mangas = ['berserk', 'onepiece', 'GTO'];
+// console.log(mangas)
+
+
+
+// function joinWithCharacter(array, charactor) {
+//     return array.join(charactor);
+// }
+
+// // Ví dụ khi sử dụng
+// var cars = ['Honda', 'Mazda', 'Mercedes'];
+
+// var result = joinWithCharacter(cars, ' - ');
+
+// console.log(result); // Expected: "Honda - Mazda - Mercedes"
+
+// function getLastElement(x){
+//     return x.slice(-1, 0);
+// }
+
+// // Ví dụ sử dụng
+// var animals = ['Monkey', 'Tiger', 'Elephant'];
+// var result = getLastElement(animals);
+
+// console.log(result); // Expected: "Elephant"
+// console.log(animals); // Expected: ['Monkey', 'Tiger', 'Elephant']
+
+
+
+//object trong js
+// var myInfo = {
+//     name: 'Nguyen Ba Duy',
+//     age: 20,
+//     email: 'email'
+// }
+
+function User(name){
+    this.name = name;
+    this.age = name;
+}
+
+var user1 = new User('Duy')
+
+console.log(user1)
